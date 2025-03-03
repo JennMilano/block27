c8f9449054a06ccb18cab45297343769b6edcfbc
Jenn Milano <jmilano0354@gmail.com>
2025-03-03 11:37:11 -0500

    halfway point

---
+++ Sign-Up/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+import Authenticate from "./components/Authenticate";
+import SignUpForm from "./components/SignUpForm";
+
+function App() {
+
+  return (
+    <>
+      <Authenticate />
+      <SignUpForm />
+    </>
+  );
+}
+
+export default App
